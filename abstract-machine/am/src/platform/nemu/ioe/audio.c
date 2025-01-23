e0ae7499b220628e0aff17adb233bc6516034982
Wizard <1749460483@qq.com>
2025-01-23 10:30:59 +0800

    before starting pa3

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -12,15 +13,82 @@ void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;//always ture
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  //AM_DEVREG(15, AUDIO_CTRL,   WR, int freq, channels, samples);
+  //set frequent
+  outl(AUDIO_FREQ_ADDR,ctrl->freq);
+  //set channels
+  outl(AUDIO_CHANNELS_ADDR,ctrl->channels);
+  //set samples
+  outl(AUDIO_SAMPLES_ADDR,ctrl->samples);
+
+  outl(AUDIO_INIT_ADDR,1);
+
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
+// void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+//   int buf_len = ctl->buf.end - ctl->buf.start;
+//   printf("buf_len = %d\n",buf_len);
+//   int count = inl(AUDIO_COUNT_ADDR);
+//   printf("count = %d\n",count);
+//   int sbuf_free = inl(AUDIO_SBUF_SIZE_ADDR) - inl(AUDIO_COUNT_ADDR);
+//   printf("sbuf_free = %d\n",sbuf_free);
+//   while(sbuf_free < buf_len){
+//   sbuf_free = inl(AUDIO_SBUF_SIZE_ADDR) - inl(AUDIO_COUNT_ADDR);
+//   }
+
+//   //get the pointer the start of free sbuf
+//   uintptr_t addr = 0xa1200000;
+//   printf("AUDIO_SBUF_ADDR = %p\n", (void *)addr);
+
+//   //uint8_t *sb = (uint8_t *)(uintptr_t)(0xa1200000 + count);
+//  uint8_t *sb = (uint8_t *)(uintptr_t)(0xa1200000 + count);
+// if (sb) {
+//     printf("sbuf_ptr = %p\n", sb);
+// } else {
+//     printf("sbuf_ptr is NULL\n");
+// }
+//   int i = 0;
+//   for(int i = 0; i < buf_len ;i++){
+//     sb[i] = ((uint8_t*)ctl->buf.start)[i] ;
+//     //printf("sb[%d]= %d\n", i, sb[i]);
+
+//   }
+//   printf("i = %d\n",i);
+//   //try to write
+//   outl(AUDIO_COUNT_ADDR,inl(AUDIO_COUNT_ADDR) + buf_len);
+// }
+
+static int sbuf_pos = 0;
+
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+	//将buf的内容作为音频数据写入流缓冲区
+//	printf("%d", sizeof(uint8_t));
+	//printf("area begin: %x, and area end: %x\n", ctl->buf, (ctl->buf).end);
+	//iprintf("sbuf_size = %d\n", inl(AUDIO_SBUF_SIZE_ADDR));
+	uint8_t *audio_data = (ctl->buf).start;
+	uint32_t sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
+	uint32_t cnt = inl(AUDIO_COUNT_ADDR);
+	uint32_t len = ((ctl->buf).end - (ctl->buf).start);
+
+	while(len > sbuf_size - cnt){;}
+
+	uint8_t *stream_buf = (uint8_t*)(uintptr_t)AUDIO_SBUF_ADDR; //参考gpu.c
+	//printf("read %d bytes\n", len);
+	for(int i = 0; i < len; i++) {
+		stream_buf[sbuf_pos] = audio_data[i];
+		//sbuf_pos = sbuf_pos + 1;
+		sbuf_pos = (sbuf_pos + 1) % sbuf_size;
+		//stream_buf[0] = audio_data[i];
+	}
+	outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + len);
+	//printf("used cnt is %d\n", inl(AUDIO_COUNT_ADDR));
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -23,15 +23,120 @@ enum {
   reg_samples,
   reg_sbuf_size,
   reg_init,
-  reg_count,
+  reg_count,//used area
   nr_reg
 };
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+int32_t count = 0;
+static int sbuf_pos = 0;
+
+// void audio_callback(void *userdata, uint8_t *stream, int len) {
+//   count = audio_base[reg_count];  // Current used bytes in the buffer
+//   int sbuf_size = CONFIG_SB_SIZE;  // Total buffer size
+
+//   //Calculate the available data that can be read from the buffer
+//   int available_data = sbuf_size - count;
+  
+//   printf("available_data = %d\n",available_data);
+//   // The actual number of bytes to read, cannot exceed the available data
+//   int nread = (count < len) ? count : len;
+//  // printf("nread = %d\n",nread);
+//   // Read audio data byte by byte from the buffer
+//   uint8_t *sbuf_ptr = (uint8_t *)(uintptr_t)(CONFIG_SB_ADDR + count);
+//   //printf("sbuf_ptr = %p\n",sbuf_ptr);
+//   int bytes_read = 0;
+//   for (int i = 0; i < nread; i++) {
+//     printf("i = %d\n",i);
+//     //printf("sbuf_ptr[i] = %d\n",sbuf_ptr[i]);
+//     //printf("stream[i] = %d\n",stream[i]);
+//     stream[i] = sbuf_ptr[i];  // Copy data from the buffer to SDL stream
+//     bytes_read++;
+//   }
+//   //printf("available_data = %d\n",available_data);
+
+//   // Update the used space in the buffer, subtract the number of bytes read
+//   audio_base[reg_count] = count - bytes_read;
+
+//   // If the requested data length is greater than the available data, fill the remaining space with zeros
+//   if (len > nread) {
+//     memset(stream + nread, 0, len - nread);  // Zero padding for remaining space
+//   }
+// }
+// use ringbuf
+void audio_sdl_callback(void *userdata, uint8_t *stream, int len) {
+	SDL_memset(stream, 0, len);
+	uint32_t used_cnt = audio_base[reg_count];
+	len = len > used_cnt ? used_cnt : len;
+	uint32_t sbuf_size = audio_base[reg_sbuf_size];
+
+	if( (sbuf_pos + len) > sbuf_size) {
+		SDL_MixAudio(stream, sbuf + sbuf_pos, sbuf_size - sbuf_pos, SDL_MIX_MAXVOLUME);
+		SDL_MixAudio(stream + (sbuf_size - sbuf_pos), sbuf + (sbuf_size - sbuf_pos), len - (sbuf_size - sbuf_pos), SDL_MIX_MAXVOLUME);
+		//printf("the length of data over the sbuf_size\n");
+	}
+	else
+		SDL_MixAudio(stream, sbuf + sbuf_pos, len, SDL_MIX_MAXVOLUME);
+	sbuf_pos = (sbuf_pos + len) % sbuf_size;
+	//printf("when run callback, the used count is %d\n--------\n", audio_base[reg_count]);
+	audio_base[reg_count] -= len;
+}
+
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  if (is_write) {
+    switch (offset/4) {
+		case reg_freq:
+		//Log("Audio frequent is %d\n",audio_base[reg_freq]);
+		break;
+		case reg_channels:
+		//Log("Audio channel is %d\n",audio_base[reg_channels]);
+		break;
+		case reg_samples:
+		//Log("Audio samples is %d\n",audio_base[reg_samples]);
+		break;	
+		case reg_init:
+        // 初始化 SDL 音频系统
+        SDL_AudioSpec s = {};
+        s.format = AUDIO_S16SYS;  
+        s.freq = audio_base[reg_freq];
+        //printf("The frequency is %d\n",audio_base[reg_freq]);
+        s.channels = audio_base[reg_channels];  
+        s.samples = audio_base[reg_samples];  
+        s.callback = audio_sdl_callback; 
+        count = 0;
+        int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
+        if (ret == 0) {
+        SDL_OpenAudio(&s, NULL);
+        SDL_PauseAudio(0);
+        //Log("Audio initial finished!\n");
+        }
+		audio_base[reg_init] = 0;
+        break;
+     case reg_count:
+        // 读取已使用大小
+        count = audio_base[reg_count] ;
+		break;
+        //printf("Count is %d",audio_base[reg_count]);
+      default:
+        Log("Unknown audio register write: offset = 0x%x", offset);
+    }
+  } else {
+    switch (offset/4) {
+      case reg_sbuf_size:
+        audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+        break;
+      case reg_count:
+        //audio_base[reg_count] = count;
+		//modify in audio_sdl_callback(),so nothing to do 
+        break;
+      default:
+        Log("Unknown audio register read: offset = 0x%x", offset);
+    }
+  }
 }
+
 
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
@@ -44,4 +149,5 @@ void init_audio() {
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+
 }
